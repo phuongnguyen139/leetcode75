24e7df9af5caeba3c7ad6fda3d08d01314d58498
phuongnguyen139 <bapcaisach@gmail.com>
2025-07-28 14:00:34 +0700

    Leetcode 75: Problems 345 and 605

---
+++ src/leetcode75/Leet75_345.java
@@ -0,0 +1,58 @@
+package leetcode75;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 345. Reverse Vowels of a String
+ * Given a string s, reverse only all the vowels in the string and return it.
+ * The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+    Example 1:
+    Input: s = "IceCreAm"
+    Output: "AceCreIm"
+    Explanation:
+    The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+    Example 2:
+    Input: s = "leetcode"
+    Output: "leotcede"
+ */
+public class Leet75_345 {
+    public static void main(String[] args) {
+        // String s = "Marge, let's \"went.\" I await news telegram.";
+        String s = " apG0i4maAs::sA0m4i0Gp0";
+        System.out.println(reverseVowels(s));
+    }
+
+    public static String reverseVowels(String s) {
+        char charS[] = s.toCharArray();
+        int pointerAsc = 0;
+        int pointerDesc = charS.length - 1;
+
+
+        while ((pointerAsc < charS.length) && (pointerDesc >= 0) && (pointerAsc <= pointerDesc)) {
+            if (isVowels(charS[pointerAsc]) && isVowels(charS[pointerDesc])) {
+                char temp;
+                temp = charS[pointerAsc];
+                charS[pointerAsc] = charS[pointerDesc];
+                charS[pointerDesc] = temp;
+                pointerAsc++;
+                pointerDesc--;
+            } else if (!isVowels(charS[pointerAsc])) {
+                pointerAsc++;
+            } else if (!isVowels(charS[pointerDesc])) {
+                pointerDesc--;
+            }
+        }
+
+        return new String(charS);
+    }
+
+    public static boolean isVowels(char c) {
+        return c == ('a') || c == ('e')
+                || c == ('i') || c == ('o') || c == ('u')
+                || c == ('A') || c == ('E')
+                || c == ('I') || c == ('O') || c == ('U');
+    }
+}
---
+++ src/leetcode75/Leet75_605.java
@@ -0,0 +1,61 @@
+package leetcode75;
+
+/**
+ * 605. Can Place Flowers
+ * You have a long flowerbed in which some of the plots are planted, and some
+ * are not. However, flowers cannot be planted in adjacent plots.
+ * Given an integer array flowerbed containing 0's and 1's, where 0 means empty
+ * and 1 means not empty, and an integer n, return true if n new flowers can be
+ * planted in the flowerbed without violating the no-adjacent-flowers rule and
+ * false otherwise.
+ * Example 1:
+ * Input: flowerbed = [1,0,0,0,1], n = 1
+ * Output: true
+ * Example 2:
+ * Input: flowerbed = [1,0,0,0,1], n = 2
+ * Output: false
+ */
+public class Leet75_605 {
+    public static void main(String[] args) {
+        // int arr[] = {1,0,0,0,1};
+        int arr[] = {1,0,0,0,1,0,0};
+        // int arr[] = {1,0,0,0,1};
+        int n = 2;
+        System.out.println(canPlace(arr, n));
+    }
+
+    public static boolean canPlace(int flowerbed[], int n) {
+        if (n == 0) {
+            return true;
+        }
+        if (flowerbed.length == 1) {
+            return flowerbed[0] == 0;
+        }
+        int numberOfPlace = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] != 1) {
+                if (i == 0) {
+                    if (flowerbed[i + 1] == 0) {
+                        flowerbed[i] = 1;
+                        numberOfPlace++;
+                    }
+                    continue;
+                }
+
+                if (i == flowerbed.length - 1 && flowerbed[i - 1] == 0) {
+                    flowerbed[i] = 1;
+                    numberOfPlace++;
+                    continue;
+                }
+                if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                    flowerbed[i] = 1;
+                    numberOfPlace++;
+                }
+            }
+        }
+
+        return n <= numberOfPlace;
+    }
+}
